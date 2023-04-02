5edac567551c9afa587b9f084d9c0ec8a13953aa
Enoras <62836997+Enoras@users.noreply.github.com>
2023-04-02 16:45:37 +0300

    add new func

--- tele_bot.py
+++ tele_bot.py
@@ -3,6 +3,7 @@ import pandas as pd
 import asyncio
 import battler as bt
 from pathlib import Path
+import aiohttp;
 
 b = bt.Battler(game_cls=bt.Kalah, game_run="play_alpha_beta")
 
@@ -12,6 +13,7 @@ table = pd.DataFrame({
     'code': [],
     'score': []
 })
+table.index.name = 'id'
 save_dir = "D:/Documents/temp"
 
 
@@ -25,6 +27,15 @@ async def get_text_messages(message):
 
     elif message.text == "/start_bt":
         await start_battle()
+    elif message.text == "/save_tb":
+        await save_table()
+    elif message.text == "/load_tb":
+        global table
+        table = await load_table()
+    elif message.text[0:8] == "/send_ms":
+        await send_text_mes(message.text[9:])
+    elif message.text == "/print_tb":
+        await print_table()
 
     else:
         await bot.send_message(message.from_user.id, "Я тебя не понимаю.")
@@ -45,31 +56,42 @@ async def get_doc_messages(message):
 
 
 def register(message):
-    table.loc[message.from_user.id] = [message.text[9:len(message.text)], ' ', 0]
+    table.loc[str(message.from_user.id)] = [message.text[9:len(message.text)], ' ', 0]
     print(table)
 
 
-def save_table():
+async def save_table():
     table.to_csv('dsl_table.csv')
 
 
-def load_table():
-    temp_table = pd.read_csv('dsl_table.csv', sep=',')
+async def load_table():
+    temp_table = pd.read_csv('dsl_table.csv', sep=',', index_col ='id')
+    print(temp_table)
     return temp_table
 
+async def print_table():
+    print(table)
+
+async def send_text_mes(mes):
+    for user in table.index:
+        await bot.send_message(user, mes)
 
 def run_test():
     print("тест")
 
 
-def send_result(res):
-    print(res)
+async def send_result(res):
+    for item in res.items():
+        table.loc[item[0],'score'] = item[1]
+        await bot.send_message(item[0], 'Ваш результат в последнем турнире: %.1f' %item[1])
 
 
 async def start_battle():
+    await send_text_mes('Раунд начинается, ваше последнее решение примет участие в турнире')
     await b.check_contestants(Path("./mail_test"), func_name="func")
     await b.run_tournament(n_workers=4, timeout=2.5)
-    await b.form_results()
+    res = await b.form_results();
+    await send_result(res)
     await b.save_results(Path("result.json"))
 
 
