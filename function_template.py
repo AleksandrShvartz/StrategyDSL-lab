a38de63c20bb8287732ab4eff28fa59b615a2db9
lyphrowny <strukov.as@edu.spbstu.ru>
2023-04-05 15:39:23 +0300

    Fix template typos

--- function_template.py
+++ function_template.py
@@ -4,13 +4,13 @@
 # 3) Please do not try to hack us.
 
 
-def func(state: list[int]) -> int:
+def func(state: list[int]) -> float:
     """Implementation of minimax strategy
 
     :param state: list[int]
         state[0:6] -- your pits, state[6] -- your kalah
-        state[8:13] -- opponent's pits, state[13] -- opponent's kalah
-    :return: int
+        state[7:13] -- opponent's pits, state[13] -- opponent's kalah
+    :return: float
         result for minimax
     """
 
